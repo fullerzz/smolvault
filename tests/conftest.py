3246f6bc7d95a7e59570ea76e9cc631b3a519ed7
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-03 16:33:17 -0700

    implemented test for GET /files/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
 import os
 from collections.abc import Generator
+from datetime import datetime
 from typing import Any
+from zoneinfo import ZoneInfo
 
 import boto3
 import pytest
 from fastapi.testclient import TestClient
 from moto import mock_aws
 from mypy_boto3_s3 import S3Client
+from smolvault.clients.database import FileMetadataRecord
 from smolvault.main import app
+from smolvault.models import FileMetadata
 
 
 @pytest.fixture(scope="session")
@@ -52,3 +56,20 @@ def test_client() -> TestClient:
 def camera_img() -> bytes:
     with open("tests/mock_data/camera.png", "rb") as f:
         return f.read()
+
+
+@pytest.fixture(scope="session")
+def file_metadata_record() -> FileMetadataRecord:
+    return FileMetadataRecord(
+        file_name="camera.png",
+        file_sha256="1234",
+        size=1234,
+        object_key="camera.png",
+        link="1234",
+        upload_timestamp=datetime.now(ZoneInfo("UTC")).isoformat(),
+    )
+
+
+@pytest.fixture(scope="session")
+def file_metadata(file_metadata_record: FileMetadataRecord) -> FileMetadata:
+    return FileMetadata.model_validate(file_metadata_record.model_dump())
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,10 +1,12 @@
 # ruff: noqa: S101
+from collections.abc import Sequence
 from typing import Any
 
 import pytest
 from httpx import AsyncClient
+from smolvault.clients.database import DatabaseClient, FileMetadataRecord
 from smolvault.main import app
-from smolvault.models import FileUploadDTO
+from smolvault.models import FileMetadata, FileUploadDTO
 
 
 @pytest.fixture(scope="module")
@@ -29,3 +31,20 @@ async def test_upload_file(client: AsyncClient, camera_img: bytes) -> None:
     actual.pop("upload_timestamp")
     assert response.status_code == 200
     assert actual == expected
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures("_test_bucket")
+async def test_list_files(
+    client: AsyncClient,
+    monkeypatch: pytest.MonkeyPatch,
+    file_metadata_record: FileMetadataRecord,
+    file_metadata: FileMetadata,
+) -> None:
+    def mock_get_all_files(*args: Any, **kwargs: Any) -> Sequence[FileMetadataRecord]:
+        return [file_metadata_record]
+
+    monkeypatch.setattr(DatabaseClient, "get_all_metadata", mock_get_all_files)
+    response = await client.get("/files/")
+    assert response.status_code == 200
+    assert response.json() == [file_metadata.model_dump(by_alias=True)]
