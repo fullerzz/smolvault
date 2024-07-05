8026b6765e1d7190096fe35433b95c54941900a6
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-05 14:33:35 -0700

    fix: removed unnecessary fixture

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -33,7 +33,7 @@ async def test_list_files(
 
 
 @pytest.mark.asyncio()
-@pytest.mark.usefixtures("_bucket_w_camera_img", "_aws_credentials")
+@pytest.mark.usefixtures("_bucket_w_camera_img")
 async def test_get_file(
     client: AsyncClient,
     monkeypatch: pytest.MonkeyPatch,
