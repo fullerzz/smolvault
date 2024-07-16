e46df109f0d128d1c6cb11faa311e2d5bc1c0e26
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-15 18:05:45 -0700

    hypercorn implementation works yay

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-poetry run uvicorn src.smolvault.main:app --log-config=log_conf.yaml --host 0.0.0.0 --server-header --timeout-keep-alive=120
-
+# poetry run uvicorn src.smolvault.main:app --log-config=log_conf.yaml --host 0.0.0.0 --server-header --timeout-keep-alive=120
+poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug
