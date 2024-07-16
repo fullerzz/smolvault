294a9d1dfb0bf0cf2b73e4d28141e1a5f5e25d5c
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-15 18:23:17 -0700

    logs shoowing up in /tmp

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # poetry run uvicorn src.smolvault.main:app --log-config=log_conf.yaml --host 0.0.0.0 --server-header --timeout-keep-alive=120
-poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug
+poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug --log-config=logging.conf --log-level=DEBUG --access-logfile=access.log --error-logfile=error.log --keep-alive=120 --workers=1
