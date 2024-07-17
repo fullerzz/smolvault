9c7b7ff31d7fb4bf1f5430d1ba3bf36f1a0b2632
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-16 17:40:31 -0700

    removed guincorn start script and renamed other script to start_app

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# poetry run uvicorn src.smolvault.main:app --log-config=log_conf.yaml --host 0.0.0.0 --server-header --timeout-keep-alive=120
 poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug --log-config=logging.conf --log-level=DEBUG --access-logfile=hypercorn.access.log --error-logfile=hypercorn.error.log --keep-alive=120 --workers=1
--- scripts/start_w_gunicorn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-poetry run gunicorn src.smolvault.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
