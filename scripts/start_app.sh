b984b6764ec8028e002fd139e763935593d44c45
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-16 17:48:12 -0700

    v0.2.0 - Hypercorn ASGI (#36)
    * simplified log output
    
    * set root log level to debug. disabled sql engine echo
    
    * updated gitignore
    
    * Hypercorn (#35)
    
    * runs w gunicorn
    
    * updated starter script
    
    * uvicorn logs now collected in uvicorn.log file
    
    * added log conf
    
    * added host option
    
    * misc updates
    
    * hypercorn implementation works yay
    
    * logs shoowing up in /tmp
    
    * updated log conf
    
    * now logs to files in local dir
    
    * removed guincorn start script and renamed other script to start_app
    
    * updated deps

---
+++ scripts/start_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug --log-config=logging.conf --log-level=DEBUG --access-logfile=hypercorn.access.log --error-logfile=hypercorn.error.log --keep-alive=120 --workers=1
