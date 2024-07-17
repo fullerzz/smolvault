f1f32149814c20e567d9042d843e00fe6f2a0846
Zach Fuller <zach.fuller1222@gmail.com>
2024-07-16 18:40:00 -0700

    updated starter script to use 2 workers

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug --log-config=logging.conf --log-level=DEBUG --access-logfile=hypercorn.access.log --error-logfile=hypercorn.error.log --keep-alive=120 --workers=1
+poetry run hypercorn src.smolvault.main:app -b 0.0.0.0 --debug --log-config=logging.conf --log-level=DEBUG --access-logfile=hypercorn.access.log --error-logfile=hypercorn.error.log --keep-alive=120 --workers=2
+
