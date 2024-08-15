96035e5366af84056701a67db50edb9b91abc68c
Zach Fuller <zach.fuller1222@gmail.com>
2024-08-14 19:37:16 -0700

    added additional assertion to test

--- tests/test_cache_manager.py
+++ tests/test_cache_manager.py
@@ -5,6 +5,7 @@ from smolvault.cache.cache_manager import CacheManager
 
 def test_create_cache_manager_dir_not_exists(tmp_path: Path) -> None:
     cache_dir = tmp_path / "cache"
+    assert cache_dir.exists() is False
     cache_mgr = CacheManager(cache_dir.as_posix())
     assert cache_mgr.cache_dir == cache_dir
     assert cache_mgr.cache_dir.exists()
