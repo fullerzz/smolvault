ebc9182ea53d3ab2531f19bf8956f085aba890ca
Zach Fuller <zach.fuller1222@gmail.com>
2024-09-21 11:30:46 -0700

    initial tf commit (#65)

---
+++ terraform/iam.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_user" "smolvault-user" {
+  name = "smolvault-user-${var.env}"
+}
+
+resource "aws_iam_access_key" "smolvault-user" {
+  user = aws_iam_user.smolvault-user.name
+}
+
+data "aws_iam_policy_document" "smolvault-policy" {
+  statement {
+    effect    = "AllowS3"
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.smolvault.arn]
+  }
+  statement {
+    effect    = "AllowKMS"
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "smolvault-user-policy" {
+  name   = "smolvault-user-policy-${var.env}"
+  user   = aws_iam_user.smolvault-user.name
+  policy = data.aws_iam_policy_document.smolvault-policy.json
+}
